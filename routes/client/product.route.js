165a12563c4b0743f25e48861a08da560f11a1ac
Thunggg <thuannguyen20041028@gmail.com>
2024-08-02 22:39:25 +0700

    Tạo folder controller

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const controller = require("../../controllers/client/product.controller");
 
-router.get('/', (req, res) => {
-    res.render("client/pages/products/index.pug");
-});
+router.get('/', controller.index);
 
 // router.post('/create', (req, res) => {
 //     res.render("client/pages/products/index.pug");
