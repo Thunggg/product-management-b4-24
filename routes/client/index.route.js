eb29dc7caa07c0a7c26bce5eca0117a899dfe145
Thunggg <thuannguyen20041028@gmail.com>
2024-08-02 21:58:47 +0700

    Cau hinh route

---
+++ routes/client/index.route.js
@@ -0,0 +1,9 @@
+module.exports.index = (app) => { //.index la ten ham`
+    app.get('/', (req, res) => {
+        res.render("client/pages/home/index.pug");
+    });
+    
+    app.get('/products', (req, res) => {
+      res.render("client/pages/products/index.pug");
+    });
+}
